bd8cff423480119affeca9f6437132086184e85e
Jonas Melo da Paz <jonasmelodapaz74@gmail.com>
2024-02-27 21:38:05 -0300

    update sidebar

--- src/app/features/layout/page/header/header.component.ts
+++ src/app/features/layout/page/header/header.component.ts
@@ -2,20 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: `
-  <div class="navbar">
-    <!-- Conteúdo da navbar -->
-    <p>Navbar Content</p>
-  </div>
-`,
-styles: [`
-  .navbar {
-    height: 60px;
-    background-color: #333;
-    color: #fff;
-    padding: 10px;
-  }
-`]
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
