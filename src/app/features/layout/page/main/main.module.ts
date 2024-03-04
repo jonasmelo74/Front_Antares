7a440b673d3a90c7ff33cf4146462f4626aa2dd8
Jonas Melo da Paz <jonasmelodapaz74@gmail.com>
2024-03-04 08:04:20 -0300

    criação do componente main

--- src/app/features/layout/page/main/main.module.ts
+++ src/app/features/layout/page/main/main.module.ts
@@ -1,28 +1,19 @@
 import { AppRoutingModule } from "src/app/app-routing.module";
-import { FooterComponent } from "./footer/footer.component";
-import { HeaderComponent } from "./header/header.component";
-import { HomeComponent } from "./home/home.component";
-import { SidebarComponent } from "./sidebar/sidebar.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { MainComponent } from './main/main.component';
+import { MainComponent } from './main.component';
 
 @NgModule({
   declarations: [
-    HomeComponent,
-    SidebarComponent,
-    FooterComponent,
-    HeaderComponent,
     MainComponent,
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    CommonModule,
-    PageModule
+    CommonModule
   ],
   providers: [],
   bootstrap: []
