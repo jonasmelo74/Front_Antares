fa1c6bdfb53416d9bdc5c9ff4157304301b4abcf
Jonas Melo da Paz <jonasmelodapaz74@gmail.com>
2024-03-07 08:03:48 -0300

    Criando tela de dashboard

--- src/app/features/layout/page/main/dashboard-main/dashboard-main.component.ts
+++ src/app/features/layout/page/main/dashboard-main/dashboard-main.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard-main',
   templateUrl: './dashboard-main.component.html',
   styleUrls: ['./dashboard-main.component.scss']
 })
-export class DashboardMainComponent {
+export class DashboardMainComponent implements OnInit {
+
+  // form: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    // this.form = this.fb.group({
+    //   name: ['', Validators.required],
+    //   email: ['', [Validators.required, Validators.email]],
+    //   message: ['', Validators.minLength(10)]
+    // });
+  }
+
 
 }
